41327482622e70f27dfcecba17ce434baf9e88fb
luke_lu <luke_lu@allen-lus-MacBook-Pro.local>
2018-03-18 21:19:40 +0800

    glide

--- src/server/main.go
+++ src/server/main.go
@@ -2,27 +2,30 @@ package main
 
 import (
 	"github.com/liangdas/mqant"
-	"server/chat"
-	"server/gate"
-	"server/login"
-	"server/hitball"
-	"server/user"
-	"webapp"
 	"github.com/liangdas/mqant/module/modules"
+	"github.com/liangdas/mqantserver/src/server/chat"
+	"github.com/liangdas/mqantserver/src/server/gate"
+	"github.com/liangdas/mqantserver/src/server/hitball"
+	"github.com/liangdas/mqantserver/src/server/login"
+	"github.com/liangdas/mqantserver/src/server/user"
+	"github.com/liangdas/mqantserver/src/webapp"
 	"sourcegraph.com/sourcegraph/appdash"
 	//appdashtracer "sourcegraph.com/sourcegraph/appdash/opentracing"
 	"github.com/opentracing/opentracing-go"
 	//"github.com/liangdas/mqant-modules/tracing"
 )
-var(
-	collector *appdash.RemoteCollector= nil
+
+var (
+	collector *appdash.RemoteCollector = nil
 
 	// Here we use the local collector to create a new opentracing.Tracer
-	tracer opentracing.Tracer= nil
+	tracer opentracing.Tracer = nil
 )
-func DefaultTracer()opentracing.Tracer{
+
+func DefaultTracer() opentracing.Tracer {
 	return tracer
 }
+
 //func ChatRoute( app module.App,Type string,hash string) (*module.ServerSession){
 //	//演示多个服务路由 默认使用第一个Server
 //	log.Debug("Hash:%s 将要调用 type : %s",hash,Type)
@@ -47,13 +50,12 @@ func main() {
 	app.Run(true, //只有是在调试模式下才会在控制台打印日志, 非调试模式下只在日志文件中输出日志
 		modules.MasterModule(),
 		hitball.Module(),
-		mgate.Module(),  //这是默认网关模块,是必须的支持 TCP,websocket,MQTT协议
+		mgate.Module(), //这是默认网关模块,是必须的支持 TCP,websocket,MQTT协议
 		login.Module(), //这是用户登录验证模块
 		chat.Module(),
 		user.Module(),
 		webapp.Module(),
 		//tracing.Module(), //很多初学者不会改文件路径，先移除了
-	)  //这是聊天模块
+	) //这是聊天模块
 
 }
-
